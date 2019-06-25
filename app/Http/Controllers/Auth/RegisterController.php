ed561a2ab719de07c12160d1c4b5cb58a3973838
Michael Vreeken <mike@vreeken.dev>
2019-06-24 18:50:52 -0700

    Update to use custom validation

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
+use App\Models\Members\User;
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+
+use Log;
+
 
 class RegisterController extends Controller
 {
@@ -28,8 +33,10 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/member';
-
+    protected $redirectTo = '/members/account';
+	protected function redirectTo() {
+		return '/members/account';
+	}
     /**
      * Create a new controller instance.
      *
@@ -40,14 +47,96 @@ class RegisterController extends Controller
         $this->middleware('guest');
     }
 
+    protected function formatPhone($phone) {
+    	if (strlen($phone) < 10) {
+			return '';
+		}
+    	$phone = preg_replace("/[^0-9]/", "", $phone);
+
+    	if ($phone[0] == '1') {
+			$phone = substr($phone, 1);
+		}
+
+    	if (strlen($phone) !== 10) {
+			return '';
+		}
+
+    	return $phone;
+	}
+
+	/**
+	 * Handle a registration request for the application.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function register(Request $request) {
+		$passcode = 'lals';
+
+		//$validator = $this->validator($request->all())->validate();
+
+		$data = $request->all();
+		$data['phone'] = $this->formatPhone($data['phone']);
+
+		$validator = Validator::make($data, [
+			'first_name' => ['required', 'string', 'max:255'],
+			'last_name' => ['required', 'string', 'max:255'],
+			'username' => ['required', 'string', 'max:255', 'unique:users'],
+			'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+			'password' => ['required', 'string', 'min:6', 'confirmed'],
+			'phone' => ['required', 'string', 'min:10', 'max:20'],
+			'phone_ext' => ['max:6'],
+			'address_1' => ['required', 'string', 'max:255'],
+			'address_2' => ['nullable', 'max:255'],
+			'city' => ['required', 'string', 'max:255'],
+			'state' => ['required', 'string', 'max:255'],
+			'postal_code' => ['required', 'string', 'min:5', 'max:10'],
+			'passcode' => ['required', 'string', 'in:'.$passcode]
+		]);
+
+		if ($validator->fails()) {
+			if ($request->expectsJson()) {
+				return response()->json(['errors' => $validator->errors()], 422);
+			}
+			else {
+				return redirect()->back()->withErrors($validator->errors());
+			}
+		}
+
+
+
+		event(new Registered($user = $this->create($request->all())));
+
+		if ($user) {
+			$this->guard()->login($user);
+			if ($request->expectsJson()) {
+				return response()->json(['success' => 'success', 'redirect'=>$this->redirectTo]);
+			}
+			else {
+				return redirect($this->redirectTo);
+			}
+		}
+
+		if ($request->expectsJson()) {
+			return response()->json(['error'=>'failed_to_register_user'], 500);
+		}
+		else {
+			return Redirect::back()->withErrors(['error', 'Failed to register user']);
+		}
+
+	}
+
+
     /**
      * Get a validator for an incoming registration request.
      *
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    protected function validator(array $data) {
+    /*protected function validator(array $data) {
     	$passcode = 'lals';
+
+    	$data['phone'] = $this->formatPhone($data['phone']);
 
         return Validator::make($data, [
             'first_name' => ['required', 'string', 'max:255'],
@@ -55,17 +144,16 @@ class RegisterController extends Controller
 			'username' => ['required', 'string', 'max:255', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
-			'phone' => ['required', 'string', 'min:10', 'max:20'],
-			'phone_ext' => ['string', 'max:6'],
+			//'phone' => ['required', 'string', 'min:10', 'max:20'],
+			'phone_ext' => ['max:6'],
 			'address_1' => ['required', 'string', 'max:255'],
 			'address_2' => ['nullable', 'max:255'],
-			'address_3' => ['nullable', 'max:255'],
 			'city' => ['required', 'string', 'max:255'],
 			'state' => ['required', 'string', 'max:255'],
-			'zip' => ['required', 'string', 'min:5', 'max:10'],
+			'postal_code' => ['required', 'string', 'min:5', 'max:10'],
 			'passcode' => ['required', 'string', 'in:'.$passcode]
         ]);
-    }
+    }*/
 
     /**
      * Create a new user instance after a valid registration.
@@ -77,6 +165,15 @@ class RegisterController extends Controller
     	/*if ($data['passcode'] !== 'lals') {
     		return ''
 		}*/
+		//$data[)->validate($this->validator());
+		/*$validation = $this->validator($request->all());
+		if ($validation->fails())  {
+			return response()->json($validation->errors()->toArray(), 422);
+		}*/
+
+
+		$data['phone'] = $this->formatPhone($data['phone']);
+
         return User::create([
             'first_name' => $data['first_name'],
 			'last_name' => $data['last_name'],
@@ -84,12 +181,12 @@ class RegisterController extends Controller
 			'username' => $data['username'],
             'password' => Hash::make($data['password']),
 			'phone' => $data['phone'],
+			'phone_ext' => $data['phone_ext'],
 			'address_1' => $data['address_1'],
 			'address_2' => $data['address_2'],
-			'address_3' => $data['address_3'],
 			'city' => $data['city'],
 			'state' => $data['state'],
-			'zip' => $data['zip']
+			'postal_code' => $data['postal_code']
         ]);
     }
 }
