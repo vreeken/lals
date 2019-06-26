6c922b987f81464bbcc47e80aed3bb4f6e18490c
Michael Vreeken <mike@vreeken.dev>
2019-06-26 15:15:20 -0700

    Invoices container vue component

---
+++ resources/js/components/members/Invoices.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<div v-for="(invoice, index) in invoices">
+			<invoice :invoice="invoice" :key="index"></invoice>
+		</div>
+	</div>
+</template>
+
+<script>
+
+	export default {
+		props: ['invoices'],
+		data() {
+			return {
+
+			}
+		},
+		mounted() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
